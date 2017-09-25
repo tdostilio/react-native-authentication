1101dae5c2b715ac059985cf48362922f8e18550
tdostilio <tdostilio@gmail.com>
2017-09-25 10:43:55 -0400

    stopped spinner from running indefinitely

--- src/App.js
+++ src/App.js
@@ -3,39 +3,38 @@ import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
-import Animation from 'lottie-react-native';
-import rey from './rey_updated.json';
+// import Animation from 'lottie-react-native';
+// import rey from './rey_updated.json';
 
 class App extends Component {
-      
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: 'AIzaSyDl1O_SdTKuITUVNy-LV3PST5wLvmwVUuo',
-            authDomain: 'authentication-9042a.firebaseapp.com',
-            databaseURL: 'https://authentication-9042a.firebaseio.com',
-            projectId: 'authentication-9042a',
-            storageBucket: 'authentication-9042a.appspot.com',
-            messagingSenderId: '317535350921'
+                apiKey: 'AIzaSyDl1O_SdTKuITUVNy-LV3PST5wLvmwVUuo',
+                authDomain: 'authentication-9042a.firebaseapp.com',
+                databaseURL: 'https://authentication-9042a.firebaseio.com',
+                projectId: 'authentication-9042a',
+                storageBucket: 'authentication-9042a.appspot.com',
+                messagingSenderId: '317535350921'
           });
     }
 
-    componentDidMount() {
-        this.animation.play();
-      }
+    // componentDidMount() {
+    //     this.animation.play();
+    //   }
 
     render() {
         return (
             <View>
-                <Header headerText="Authentication" />
+                <Header headerText='Authentication' />
                 <LoginForm />
-                <Animation
+                {/* <Animation
                     ref={animation => { this.animation = animation; }}
                     style={{
                     width: 300,
                     height: 300,
                     }}
                     source={require('./rey_updated.json')}
-                />
+                /> */}
             </View>
 
         );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,12 +14,24 @@ class LoginForm extends Component {
         this.setState({ error: '', loading: true });
 
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .catch(() => {
-                        this.setState({ error: 'Authentication Failed.' });
-                    });
+                .then(this.onLoginSuccess.bind(this))
+                .catch(this.onLoginFail.bind(this));
             });
+    }
+
+    onLoginFail() {
+        this.setState({ error: 'Authentication Failed', loading: false });
+    }
+    onLoginSuccess() {
+        this.setState({ 
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+         });
     }
 
     renderButton() {
